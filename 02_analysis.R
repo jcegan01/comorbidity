49c66bbe7dc45df510f42df17bb7b12f7318ec36
Jeff Cegan <Cegan@Jeff-Cegans-MBP.fios-router.home>
2020-05-25 17:09:58 -0400

    commit

--- 02_analysis.R
+++ 02_analysis.R
@@ -18,51 +18,58 @@ summary(cmr)
 
 ######### analysis ######### 
 
+#total population in each group
+cmr %>% group_by(age_group, comor_count) %>% tally() 
+
 #overall table
-res<- cmr %>%
+res_overall <- cmr %>%
   group_by(age_group, comor_count) %>%
   mutate(count = n()) %>% 
   summarize(population = mean(count),
-            death_rate = mean(death_yn, na.rm = TRUE),
             hosp_rate = mean(hosp_yn, na.rm = TRUE),           
             icu_rate = mean(icu_yn, na.rm = TRUE),
-            mechvent_rate = mean(mechvent_yn, na.rm = TRUE)) 
+            mechvent_rate = mean(mechvent_yn, na.rm = TRUE),
+            death_rate = mean(death_yn, na.rm = TRUE)) 
 
-cmr %>% group_by(age_group, comor_count) %>% tally() #total population in each group
-  
 #conditional if in HOSP
-cmr %>%
+res_hosp <- cmr %>%
   group_by(age_group, comor_count) %>%
   filter(hosp_yn == 1) %>%
   mutate(count = n()) %>% 
   summarize(population = mean(count),
-            death_rate = mean(death_yn, na.rm = TRUE),
             hosp_rate = mean(hosp_yn, na.rm = TRUE),           
             icu_rate = mean(icu_yn, na.rm = TRUE),
-            mechvent_rate = mean(mechvent_yn, na.rm = TRUE)) 
+            mechvent_rate = mean(mechvent_yn, na.rm = TRUE),
+            death_rate = mean(death_yn, na.rm = TRUE)) 
 
 #conditional if in ICU
-cmr %>%
+res_icu <- cmr %>%
   group_by(age_group, comor_count) %>%
   filter(icu_yn == 1) %>%
   mutate(count = n()) %>% 
   summarize(population = mean(count),
-            death_rate = mean(death_yn, na.rm = TRUE),
             hosp_rate = mean(hosp_yn, na.rm = TRUE),           
             icu_rate = mean(icu_yn, na.rm = TRUE),
-            mechvent_rate = mean(mechvent_yn, na.rm = TRUE)) 
+            mechvent_rate = mean(mechvent_yn, na.rm = TRUE),
+            death_rate = mean(death_yn, na.rm = TRUE)) 
 
 #conditional if Ventilated
-cmr %>%
+res_vent <- cmr %>%
   group_by(age_group, comor_count) %>%
   filter(mechvent_yn == 1) %>%
   mutate(count = n()) %>% 
   summarize(population = mean(count),
-            death_rate = mean(death_yn, na.rm = TRUE),
             hosp_rate = mean(hosp_yn, na.rm = TRUE),           
             icu_rate = mean(icu_yn, na.rm = TRUE),
-            mechvent_rate = mean(mechvent_yn, na.rm = TRUE)) 
+            mechvent_rate = mean(mechvent_yn, na.rm = TRUE),
+            death_rate = mean(death_yn, na.rm = TRUE)) 
+
 
 ######### output ######### 
-write.csv(res, "outputs/comorbidity_results.csv",row.names = FALSE)
+
+write.csv(res_overall, paste0("outputs/comorbidity_overall_",format(Sys.time(), "%Y-%m-%d"),".csv"),row.names = FALSE)
+write.csv(res_hosp, paste0("outputs/comorbidity_hosp_",format(Sys.time(), "%Y-%m-%d"),".csv"),row.names = FALSE)
+write.csv(res_icu, paste0("outputs/comorbidity_icu_",format(Sys.time(), "%Y-%m-%d"),".csv"),row.names = FALSE)
+write.csv(res_vent, paste0("outputs/comorbidity_vent_",format(Sys.time(), "%Y-%m-%d"),".csv"),row.names = FALSE)
+
   
